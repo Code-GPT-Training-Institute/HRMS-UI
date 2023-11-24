d38e1c08a6c79516a7c5f85265879c653597be73
udayakumar <pnrudaya@gmail.com>
2023-11-24 23:32:40 +0530

    login without action completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,17 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  public username: string = '';
-  public password: string = '';
-  public enableLoginButton: Boolean = false;
+  //  public username: string = '';
+  //  public password: string = '';
+  public enableLoginButton: Boolean = true;
+  loginUserForm : FormGroup = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl('')
+  });
+  submitted = false;
   
-  constructor(){}
+  constructor(private formBuilder: FormBuilder){}
 
-  public showUserName() {    
-    if(this.username !== undefined && this.username !== "" && this.password !== undefined && this.password !== "" ){
-      alert(`This is your username:  ${this.username}`)
-    } else {
-      alert("Please give the username and password")
-    }    
+  ngOnInit(): void{
+    this.loginUserForm = this.formBuilder.group({
+      userName: [
+        '',[
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-zA-Z0-9+_.-]+@codegpt.com+$')
+        ],
+      ],
+      password: [
+        '', [
+          Validators.required,
+          //Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{6,12}$"),
+          Validators.pattern('^(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{6,12}$'),
+          Validators.minLength(6),
+          Validators.maxLength(12),          
+        ],
+      ]
+    })
   }
+
+  
+  get f(){
+    return this.loginUserForm.controls;
+  }
+  
+  loginSubmit(){
+    this.submitted = true;    
+
+    if (this.loginUserForm.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.loginUserForm.value, null, 2));
+  }
+
+  // get loginpassword(){
+  // return this.loginUser.get('password')
+  // }
+//public showUserName() {    
+    // if(this.loginUser. !== undefined && this.username !== "" && this.password !== undefined && this.password !== "" ){
+    //   alert(`This is your username:  ${this.username}`)
+    // } else {
+    //   alert("Please give the username and password")
+    // }    
+//}
 }
